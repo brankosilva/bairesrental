4522e08561f7c2686e512c0018f6272dd30b2018
Branko <brankoemmanuelsilva@gmail.com>
2025-02-17 14:32:42 -0300

    perfiles

--- js/main.js
+++ js/main.js
@@ -313,6 +313,32 @@ window.addEventListener("scroll", function(){
 	nav.classList.toggle("abajo",window.scrollY>0);
 })
 
+/* CLEINTES PRUEBA */
+
+$(document).ready(function () {
+    var silder = $(".carousel-clientes");
+    silder.owlCarousel({
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: false,
+        items: 1,
+        stagePadding: 20,
+        center: true,
+        nav: false,
+        margin: 50,
+        dots: true,
+        loop: true,
+        responsive: {
+            0: { items: 1 },
+            480: { items: 2 },
+            575: { items: 2 },
+            768: { items: 2 },
+            991: { items: 3 },
+            1200: { items: 4 }
+        }
+    });
+});
+
 
 /* calculadora */
 
